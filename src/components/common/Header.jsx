51c5e9de037e1b99cf261507d2500d160f5bd1c6
Mustaque Nadim <mustaquenadim2000@gmail.com>
2021-11-09 16:33:49 +0600

    project initialization

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Computer Solution</h1>
+      <p>Computer Accessories Sales and Servicing Center</p>
+    </div>
+  );
+};
+
+export default Header;
